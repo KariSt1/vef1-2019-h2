4fc34032cadaf47c0b98b9f7aef2e1765b085aef
KariSt1 <kari_steinn@outlook.com>
2019-11-15 21:12:14 +0000

    Bætti við hugmynd af uppsetningu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import List from './lib/list';
+import { el } from './lib/helpers';
 
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
@@ -8,6 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   } else {
     const list = new List();
+    //for all children of buttons {
+    //  if(button classname === button--active) {
+    //    const name = button.name;
+    //    if (fyrirlestur.type === name) {
+    //      list.add (el('div', ...))
+    //    }
+    //  }
+    //}
     list.load();
   }
 });
