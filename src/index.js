37eda31cfec9099197592b7f5a129720cf2497ba
Kari <kari_steinn@outlook.com>
2019-11-24 17:22:15 +0000

    Tók út óþarfa imports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import List from './lib/list';
-import { el } from './lib/helpers';
+import Lecture from './lib/lecture';
 
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
   if (isLecturePage) {
-
+    const lecture = new Lecture();
+    lecture.load();
   } else {
     const list = new List();
-    //for all children of buttons {
-    //  if(button classname === button--active) {
-    //    const name = button.name;
-    //    if (fyrirlestur.type === name) {
-    //      list.add (el('div', ...))
-    //    }
-    //  }
-    //}
     list.load();
   }
 });
