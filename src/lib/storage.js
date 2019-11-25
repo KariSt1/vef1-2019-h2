5093f8fa6bb487c20f4d2f4b5e0583246f50ff3f
Kari <kari_steinn@outlook.com>
2019-11-25 22:09:48 +0000

    Fjarlægði console.log

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,16 +1,13 @@
 const LOCALSTORAGE_KEY = 'finished_lectures';
 
 export function loadFinished() {
-  console.log('Er í loadFinished');
   if (window.localStorage.getItem(LOCALSTORAGE_KEY)) {
-    console.log(window.localStorage.getItem(LOCALSTORAGE_KEY));
     return window.localStorage.getItem(LOCALSTORAGE_KEY);
   }
   return [];
 }
 
 export function save(slug, isFinished) {
-  console.log('Er í save');
   let saved = JSON.parse(window.localStorage.getItem(LOCALSTORAGE_KEY));
   if (!saved) {
     saved = [];
@@ -22,5 +19,4 @@ export function save(slug, isFinished) {
     saved.push(slug);
   }
   window.localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(saved));
-  console.log('Savaðir hlutir: ', window.localStorage.getItem(LOCALSTORAGE_KEY));
 }
